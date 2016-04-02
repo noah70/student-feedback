ad584d2a804897b56eecbbabdeb9daa8e84b9747
Noah70 <noah70@users.noreply.github.com>
2016-04-02 16:42:22 +0800

    Add modules function

---
+++ database/migrations/2016_04_02_043725_create_modules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('code');
+            $table->integer('teacher_id');
+            $table->string('semester');
+            $table->integer('year');
+            $table->string('type');
+            $table->string('secret_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('modules');
+    }
+}
